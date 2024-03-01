b1336eaa0677696f9a57d704a5cd5a929e6e5959
Sean Hamm <seanhamm28@gmail.com>
2024-02-29 22:00:52 -0800

    repushing- last push failed due to file size. adjusted

--- gallery.js
+++ gallery.js
@@ -1,45 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const slides = document.querySelectorAll(".gallery-slide");
-    let currentSlide = 0;
+  const slides = document.querySelectorAll(".gallery-slide");
+  let currentSlide = 0;
 
-    function showSlide(index) {
-        slides.forEach((slide, i) => {
-            slide.style.display = i === index ? "block" : "none";
-        });
-    }
-
-    function nextSlide() {
-        currentSlide = (currentSlide + 1) % slides.length;
-        showSlide(currentSlide);
-        updateRadioButtons();
-    }
-
-    function startAutoSlide() {
-        setInterval(nextSlide, 7000); // Change 7000 to the desired interval in milliseconds
-    }
-
-    function manualSlide(index) {
-        currentSlide = index;
-        showSlide(currentSlide);
-        updateRadioButtons();
-    }
-
-    function updateRadioButtons() {
-        const radioButtons = document.querySelectorAll('input[name="slide-radio"]');
-        radioButtons.forEach((radio, index) => {
-            radio.checked = index === currentSlide;
-        });
-    }
-
-    showSlide(currentSlide);
-    startAutoSlide();
-
-    const radioButtons = document.querySelectorAll('input[name="slide-radio"]');
-    
-    // Add event listener to each radio button
-    radioButtons.forEach((radio, index) => {
-        radio.addEventListener('change', () => {
-            manualSlide(index);
-        });
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.style.display = i === index ? "block" : "none";
     });
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+    updateRadioButtons();
+  }
+
+  function startAutoSlide() {
+    setInterval(nextSlide, 7000); // Change 7000 to the desired interval in milliseconds
+  }
+
+  function manualSlide(index) {
+    currentSlide = index;
+    showSlide(currentSlide);
+    updateRadioButtons();
+  }
+
+  function updateRadioButtons() {
+    const radioButtons = document.querySelectorAll('input[name="slide-radio"]');
+    radioButtons.forEach((radio, index) => {
+      radio.checked = index === currentSlide;
+    });
+  }
+
+  showSlide(currentSlide);
+  startAutoSlide();
+
+  const radioButtons = document.querySelectorAll('input[name="slide-radio"]');
+
+  // Add event listener to each radio button
+  radioButtons.forEach((radio, index) => {
+    radio.addEventListener("change", () => {
+      manualSlide(index);
+    });
+  });
 });
