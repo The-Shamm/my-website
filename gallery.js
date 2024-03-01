a9c1d7ef132d1bd589919106fd4ea9803783b1f0
Sean Hamm <seanhamm28@gmail.com>
2024-02-29 23:35:04 -0800

    remaining for about page is tranferring services to Homepage

--- gallery.js
+++ gallery.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-<<<<<<< HEAD
   const slides = document.querySelectorAll(".gallery-slide");
   let currentSlide = 0;
 
@@ -43,26 +42,4 @@ document.addEventListener("DOMContentLoaded", function () {
       manualSlide(index);
     });
   });
-=======
-    const slides = document.querySelectorAll(".gallery-slide");
-    let currentSlide = 0;
-
-    function showSlide(index) {
-        slides.forEach((slide, i) => {
-            slide.style.display = i === index ? "block" : "none";
-        });
-    }
-
-    function nextSlide() {
-        currentSlide = (currentSlide + 1) % slides.length;
-        showSlide(currentSlide);
-    }
-
-    function startAutoSlide() {
-        setInterval(nextSlide, 7000); // Change 3000 to the desired interval in milliseconds
-    }
-
-    showSlide(currentSlide);
-    startAutoSlide();
->>>>>>> f05452b9244783e81c5afeb27e1bc57a5815435e
 });
